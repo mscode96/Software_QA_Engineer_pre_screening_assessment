9e7bbf6c488b31c83ce3ac3c0089b22a7b0cb985
mscode96 <codemine96@gmail.com>
2017-05-17 23:39:28 +0530

    Create ArithmeticOperations.java
    ArithmeticOperations.java for class to perform simple arithmetic.

---
+++ ArithmeticOperations.java
@@ -0,0 +1,41 @@
+package Assessment;
+
+public class ArithmeticOperations {
+
+
+    //calculating number1 + number2;
+    public int getSum(int number1, int number2 ) {
+
+        int sum = number1 + number2;
+        return sum;
+    }
+
+    //calculating number1  - number2;
+    public int getDifference(int number1, int number2) {
+
+        int difference = number1 - number2;
+        return difference;
+    }
+
+    //calculating number1 * number2;
+
+    public int getProduct(int number1, int number2) {
+        int product = number1 * number2;
+        return product;
+    }
+
+    //calculating number1 / number2;
+
+    public int getDivision(int number1, int number2) {
+        int division = number1 / number2;
+        return division;
+    }
+
+    //calculating number1 % number2;
+
+    public int getRem(int number1, int number2) {
+        int rem = number1 % number2;
+        return rem;
+    }
+
+}
